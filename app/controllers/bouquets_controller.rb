194e4b51332733549f5380725c1d9a0da6a1cfa5
Andrii Ukrainets <andrii.ukraiinets@gmail.com>
2024-12-30 01:11:24 -0700

    add `new` & `create` Bouquets actions

---
+++ app/controllers/bouquets_controller.rb
@@ -0,0 +1,20 @@
+class BouquetsController < ApplicationController
+  def new
+    @bouquet = Bouquet.new
+  end
+
+  def create
+    @bouquet = Bouquet.new(bouquet_params)
+    if @bouquet.save
+      redirect_to root_path, notice: "Bouquet successfully created."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bouquet_params
+    params.require(:bouquet).permit(:name, :description, photos: [])
+  end
+end
