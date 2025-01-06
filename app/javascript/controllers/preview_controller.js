ed18bcabfb8a66e531e1367ed84d74eea4a2cd99
Andrii Ukrainets <andrii.ukraiinets@gmail.com>
2025-01-05 20:30:26 -0700

    add ability to preview uploaded files

---
+++ app/javascript/controllers/preview_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["fileInput", "preview"];
+
+  preview() {
+    console.log("preview");
+
+    const files = this.fileInputTarget.files;
+    this.previewTarget.innerHTML = "";
+
+    Array.from(files).forEach((file) => {
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        const img = document.createElement("img");
+        img.src = event.target.result;
+        img.classList.add("w-full", "h-auto", "object-cover", "rounded-lg", "shadow-md", "mb-4");
+        this.previewTarget.appendChild(img);
+      };
+
+      reader.readAsDataURL(file);
+    });
+  }
+}
